19d2fc754a802694b092dd8c41fb75462275c654
Y.Y <yy_im@outlook.com>
2025-10-05 17:56:40 +0800

    f

--- update.sh
+++ update.sh
@@ -182,7 +182,7 @@ remove_unwanted_packages() {
     )
     local small8_packages=(
         "ppp" "firewall" "dae" "daed" "daed-next" "libnftnl" "nftables" "dnsmasq" "luci-app-alist"
-        "alist" "opkg" "smartdns" "luci-app-smartdns"
+        "alist" "opkg" "smartdns" "luci-app-smartdns" "tcping"
     )
 
     for pkg in "${luci_packages[@]}"; do
